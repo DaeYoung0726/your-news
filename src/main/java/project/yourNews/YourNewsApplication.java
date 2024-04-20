e4ab346bc59a0520d762fba5df630a5ec3713f18
DaeYoung Park <pdy000726@naver.com>
2024-04-21 01:38:25 +0900

    feat - 초기 설정

---
+++ src/main/java/project/yourNews/YourNewsApplication.java
@@ -0,0 +1,13 @@
+package project.yourNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/yourNews/YourNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.yourNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
