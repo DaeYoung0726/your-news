e3e2e4244c4cc5ce60cdbe4e94e051f3f2ef68d5
DaeYoung Park <pdy000726@naver.com>
2024-05-19 16:29:53 +0900

    feat - subNews DTO 생성

---
+++ src/main/java/project/yourNews/domains/subNews/dto/SubNewsResponseDto.java
@@ -0,0 +1,15 @@
+package project.yourNews.domains.subNews.dto;
+
+import lombok.Getter;
+import project.yourNews.domains.news.domain.News;
+import project.yourNews.domains.subNews.domain.SubNews;
+
+@Getter
+public class SubNewsResponseDto {
+
+    private final News news;
+
+    public SubNewsResponseDto(SubNews subNews) {
+        this.news = subNews.getNews();
+    }
+}
