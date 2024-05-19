59aadf2001c22dcb529a2d88d7f7cae9ebb57bae
DaeYoung Park <pdy000726@naver.com>
2024-05-19 16:23:59 +0900

    feat - urlHistory Service 구현

--- src/main/java/project/yourNews/domains/urlHistory/service/URLHistoryService.java
+++ src/main/java/project/yourNews/domains/urlHistory/service/URLHistoryService.java
@@ -1,2 +1,54 @@
-package project.yourNews.domains.urlHistory.service;public class URLHistoryService {
+package project.yourNews.domains.urlHistory.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.yourNews.domains.urlHistory.domain.URLHistory;
+import project.yourNews.domains.urlHistory.dto.URLResponseDto;
+import project.yourNews.domains.urlHistory.repository.URLHistoryRepository;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class URLHistoryService {
+
+    private final URLHistoryRepository urlHistoryRepository;
+
+    /* 보낸 url 저장하기 */
+    @Transactional
+    public void saveURL(String url) {
+
+        URLHistory urlHistory = URLHistory.builder()
+                .dispatchedURL(url)
+                .build();
+
+        urlHistoryRepository.save(urlHistory);
+    }
+
+    /* 저장된 url 전체 불러오기 */
+    @Transactional(readOnly = true)
+    public List<URLResponseDto> readAllURL() {
+
+        List<URLHistory> urlHistories = urlHistoryRepository.findAll();
+        return urlHistories.stream().map(URLResponseDto::new).toList();
+    }
+
+    /* 저장된 url 삭제하기 */
+    @Transactional
+    public void deleteURL(Long urlId) {
+
+        URLHistory url = urlHistoryRepository.findById(urlId).orElseThrow(() ->
+                new IllegalArgumentException("해당하는 url이 없습니다"));
+
+        urlHistoryRepository.delete(url);
+    }
+
+    /* 이미 보낸 소식인지 확인 */
+    public boolean existsURLCheck(String url) {
+
+        return urlHistoryRepository.existsByDispatchedURL(url);
+    }
 }
