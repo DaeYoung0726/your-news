252d23397e8d7c6d9c0eceec099769628773b530
DaeYoung Park <pdy000726@naver.com>
2024-05-21 17:02:03 +0900

    feat - token 관련 에러 추가

--- src/main/java/project/yourNews/handler/exceptionHandler/error/ErrorCode.java
+++ src/main/java/project/yourNews/handler/exceptionHandler/error/ErrorCode.java
@@ -16,6 +16,9 @@ public enum ErrorCode {
     ACCESS_TOKEN_EXPIRED(401, "Access Token이 만료되었습니다."),
     INVALID_ACCESS_TOKEN(401, "Access Token이 잘못되었습니다."),
 
+    // AccessToken 관련 오류
+    BLACKLIST_ACCESS_TOKEN(401, "접근 불가한 AccessToken입니다."),
+
     // RefreshToken 인증 중 오류
     REFRESH_TOKEN_NOT_FOUND(404, "RefreshToken이 존재하지 않습니다."),
     REFRESH_TOKEN_EXPIRED(404, "RefreshToken이 만료되었습니다."),
