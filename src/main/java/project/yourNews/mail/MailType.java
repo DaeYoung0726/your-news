ef5dee98916a75589f00c3f1ead1609bfa95a2b1
DaeYoung Park <pdy000726@naver.com>
2024-05-22 19:24:27 +0900

    feat - 메일 서비스 구현

--- src/main/java/project/yourNews/mail/MailType.java
+++ src/main/java/project/yourNews/mail/MailType.java
@@ -1,2 +1,8 @@
-package project.yourNews.mail;public enum MailType {
+package project.yourNews.mail;
+
+public enum MailType {
+
+    CODE,       // 회원가입 인증 코드
+    NEWS,       // 새로운 소식 전달
+    PASS        // 임시 비밀번호 발급
 }
--- src/main/java/project/yourNews/mail/service/MailService.java
+++ src/main/java/project/yourNews/mail/service/MailService.java
@@ -1,2 +1,80 @@
-package project.yourNews.mail.service;public class MailService {
+package project.yourNews.mail.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.yourNews.mail.MailType;
+
+import static project.yourNews.mail.util.MailProperties.*;
+
+@RequiredArgsConstructor
+@Transactional
+@EnableAsync
+@Service
+public class MailService {
+
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String emailUsername;
+
+    /* 메일 보내기 */
+    @Async
+    public void sendMail(String email, String content, MailType type) {
+
+        SimpleMailMessage message = null;
+
+        switch (type) {
+            case CODE -> message = getCodeMessage(email, content);
+            case NEWS -> message = getNewsMessage(email, content);
+            case PASS -> message = getPassMessage(email, content);
+        }
+
+        try {
+            javaMailSender.send(message);
+        } catch (MailException ignored) {
+        }
+    }
+
+    /* 인증메일 보내기 */
+    public SimpleMailMessage getCodeMessage(String email, String code) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(CODE_SUBJECT);
+        message.setText(CODE_TEXT + code);
+        message.setFrom(emailUsername);
+
+        return message;
+    }
+
+    /* 소식 보내기 */
+    public SimpleMailMessage getNewsMessage(String email, String news) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(NEWS_SUBJECT);
+        message.setText(NEWS_TEXT + news);
+        message.setFrom(emailUsername);
+
+        return message;
+    }
+
+    /* 임시 비밀번호 보내기 */
+    public SimpleMailMessage getPassMessage(String email, String pass) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(PASS_SUBJECT);
+        message.setText(PASS_TEXT + pass);
+        message.setFrom(emailUsername);
+
+        return message;
+    }
 }
