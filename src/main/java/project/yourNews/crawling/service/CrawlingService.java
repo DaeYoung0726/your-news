b1ccd8c77e83d3bcc796c88b56111b38cc809785
DaeYoung Park <pdy000726@naver.com>
2024-05-23 17:11:03 +0900

    feat - 웹 크롤링 기능 구현

--- src/main/java/project/yourNews/crawling/service/CrawlingService.java
+++ src/main/java/project/yourNews/crawling/service/CrawlingService.java
@@ -1,2 +1,79 @@
-package project.yourNews.crawling.service;public class CrawlingService {
+package project.yourNews.crawling.service;
+
+import lombok.RequiredArgsConstructor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+import project.yourNews.domains.member.domain.Member;
+import project.yourNews.domains.member.service.MemberService;
+import project.yourNews.domains.news.dto.NewsResponseDto;
+import project.yourNews.domains.news.service.NewsService;
+import project.yourNews.domains.urlHistory.service.URLHistoryService;
+import project.yourNews.mail.MailType;
+import project.yourNews.mail.service.MailService;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CrawlingService {
+
+    private final NewsService newsService;
+    private final MemberService memberService;
+    private final URLHistoryService urlHistoryService;
+    private final MailService mailService;
+    private static final String SCHEDULED_TIME = "0 0 8-19 * * *";  // 1시간마다 크롤링
+
+    @Scheduled(cron = SCHEDULED_TIME, zone = "Asia/Seoul") // 오전 8시부터 오후 7시까지
+    @Transactional
+    public void startCrawling() throws IOException {
+
+        List<NewsResponseDto> news = newsService.readAllNews();
+        for (NewsResponseDto readNews: news) {
+            analyzeWeb(readNews.getNewsURL());
+        }
+    }
+
+    /* 페이지 크롤링 */
+    private void analyzeWeb(String newsURL) throws IOException {
+
+        // 웹 페이지 가져오기
+        Document doc = Jsoup.connect(newsURL).get();
+
+        // 게시글 요소 찾기
+        Elements postElements = doc.select("tr[class='']"); // class가 없는 경우
+        postElements.addAll(doc.select("tr.b-top-box")); // b-top-box 클래스를 포함하는 경우
+
+        // 게시글 제목과 URL 출력
+        for (Element postElement : postElements) {
+            Element newPostElement = postElement.selectFirst("p.b-new");
+            if (newPostElement != null) { // 새로운 게시글인 경우에만 처리
+                Element titleElement = postElement.selectFirst("div.b-title-box > a");
+                String postTitle = titleElement.text();
+                String postURL = titleElement.absUrl("href");
+
+                // 새로운 게시글인 경우에만 출력
+                if (!urlHistoryService.existsURLCheck(postURL)) {
+                    // 여기서 알림을 보내는 로직을 추가할 수 있습니다.
+                    List<Member> members = memberService.getMembersSubscribedToNews(newsURL);
+
+                    for (Member member: members) {
+                        sendNewsToMember(member.getEmail(), postTitle, postURL);
+                    }
+                    // 새로운 게시글 URL을 목록에 추가
+                    urlHistoryService.saveURL(postURL);
+                }
+            }
+        }
+    }
+
+    /* 소식 구독자에게 메일 보내기 */
+    private void sendNewsToMember(String memberEmail, String postTitle, String postURL) {
+
+        String mailContent = postTitle + '\n' + postURL;
+        mailService.sendMail(memberEmail, mailContent, MailType.NEWS);
+    }
 }
