6b1b01f065b22d4af5f2a971b2ab25145bb7a49f
DaeYoung Park <pdy000726@naver.com>
2024-05-30 01:45:29 +0900

    feat - admin이 회원 탈퇴시키기 메서드 추가

--- src/main/java/project/yourNews/domains/member/service/AdminService.java
+++ src/main/java/project/yourNews/domains/member/service/AdminService.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import project.yourNews.domains.member.domain.Member;
 import project.yourNews.domains.member.dto.MemberInfoDto;
 import project.yourNews.domains.member.repository.MemberRepository;
+import project.yourNews.handler.exceptionHandler.error.ErrorCode;
+import project.yourNews.handler.exceptionHandler.exception.CustomException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,11 +19,22 @@ public class AdminService {
 
     private final MemberRepository memberRepository;
 
+    /* 사용자 전체 불러오기 */
     @Transactional(readOnly = true)
     public Page<MemberInfoDto> findAllMembers(Pageable pageable) {
 
         Page<Member> members = memberRepository.findAll(pageable);
 
         return members.map(MemberInfoDto::new);
+    }
+
+    /* 사용자 탈퇴 */
+    @Transactional
+    public void dropMember(Long memberId) {
+
+        Member findMember = memberRepository.findById(memberId).orElseThrow(() ->
+                new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        memberRepository.delete(findMember);
     }
 }
