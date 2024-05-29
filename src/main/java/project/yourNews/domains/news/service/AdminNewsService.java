b5b9d0d3d4b86a012ece2f7f91e34097a7a326bc
DaeYoung Park <pdy000726@naver.com>
2024-05-30 02:39:34 +0900

    refactor - admin 기능 news service 나누기

--- src/main/java/project/yourNews/domains/news/service/AdminNewsService.java
+++ src/main/java/project/yourNews/domains/news/service/AdminNewsService.java
@@ -1,2 +1,44 @@
-package project.yourNews.domains.news.service;public class AdminNewsService {
+package project.yourNews.domains.news.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.yourNews.domains.news.domain.News;
+import project.yourNews.domains.news.dto.NewsRequestDto;
+import project.yourNews.domains.news.dto.NewsResponseDto;
+import project.yourNews.domains.news.repository.NewsRepository;
+import project.yourNews.handler.exceptionHandler.error.ErrorCode;
+import project.yourNews.handler.exceptionHandler.exception.CustomException;
+
+@RequiredArgsConstructor
+@Service
+public class AdminNewsService {
+
+    private final NewsRepository newsRepository;
+
+    /* 소식 생성 */
+    @Transactional
+    public void saveNews(NewsRequestDto newsDto) {
+
+        newsRepository.save(newsDto.toNewsEntity());
+    }
+
+    /* 특정 소식 불러오기 */
+    @Transactional(readOnly = true)
+    public NewsResponseDto readNews(Long newsId) {
+
+        News findNews = newsRepository.findById(newsId).orElseThrow(() ->
+                new CustomException(ErrorCode.NEWS_NOT_FOUND));
+
+        return new NewsResponseDto(findNews);
+    }
+
+    /* 소식 삭제하기 */
+    @Transactional
+    public void deleteNews(Long newsId) {
+
+        News findNews = newsRepository.findById(newsId).orElseThrow(() ->
+                new CustomException(ErrorCode.NEWS_NOT_FOUND));
+        newsRepository.delete(findNews);
+    }
 }
--- src/main/java/project/yourNews/domains/news/service/NewsService.java
+++ src/main/java/project/yourNews/domains/news/service/NewsService.java
@@ -5,11 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.yourNews.domains.news.domain.News;
-import project.yourNews.domains.news.dto.NewsRequestDto;
-import project.yourNews.domains.news.dto.NewsResponseDto;
+import project.yourNews.domains.news.dto.NewsInfoDto;
 import project.yourNews.domains.news.repository.NewsRepository;
-import project.yourNews.handler.exceptionHandler.error.ErrorCode;
-import project.yourNews.handler.exceptionHandler.exception.CustomException;
 
 import java.util.List;
 
@@ -20,37 +17,11 @@ public class NewsService {
 
     private final NewsRepository newsRepository;
 
-    /* 소식 생성 */
-    @Transactional
-    public void saveNews(NewsRequestDto newsDto) {
-
-        newsRepository.save(newsDto.toNewsEntity());
-    }
-
-    /* 특정 소식 불러오기 */
-    @Transactional(readOnly = true)
-    public NewsResponseDto readNews(Long newsId) {
-
-        News findNews = newsRepository.findById(newsId).orElseThrow(() ->
-                new CustomException(ErrorCode.NEWS_NOT_FOUND));
-
-        return new NewsResponseDto(findNews);
-    }
-
     /* 전체 소식 불러오기 */
     @Transactional(readOnly = true)
-    public List<NewsResponseDto> readAllNews() {
+    public List<NewsInfoDto> readAllNews() {
 
         List<News> news = newsRepository.findAll();
-        return news.stream().map(NewsResponseDto::new).toList();
-    }
-
-    /* 소식 삭제하기 */
-    @Transactional
-    public void deleteNews(Long newsId) {
-
-        News findNews = newsRepository.findById(newsId).orElseThrow(() ->
-                new CustomException(ErrorCode.NEWS_NOT_FOUND));
-        newsRepository.delete(findNews);
+        return news.stream().map(NewsInfoDto::new).toList();
     }
 }
