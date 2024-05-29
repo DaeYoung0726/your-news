a0dedbef2066175b75f63d71812277529e52ef8b
DaeYoung Park <pdy000726@naver.com>
2024-05-30 01:48:30 +0900

    feat - 권한 확인을 위한 aop 추가

--- src/main/java/project/yourNews/aop/VerifyAuthenticationAspect.java
+++ src/main/java/project/yourNews/aop/VerifyAuthenticationAspect.java
@@ -1,2 +1,50 @@
-package project.yourNews.aop;public class VerifyAuthenticationAspect {
+package project.yourNews.aop;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import project.yourNews.domains.post.domain.Post;
+import project.yourNews.domains.post.repository.PostRepository;
+import project.yourNews.handler.exceptionHandler.error.ErrorCode;
+import project.yourNews.handler.exceptionHandler.exception.CustomException;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class VerifyAuthenticationAspect {
+
+    private final PostRepository postRepository;
+
+    @Around("@annotation(project.yourNews.aop.annotation.VerifyAuthentication)")
+    public Object verifyAuthentication(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        Object[] args = joinPoint.getArgs();
+
+        UserDetails principalDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String expectedUsername = principalDetails.getUsername();
+
+        for (Object arg : args) {
+            if (arg instanceof Long) {
+                Long postId = (Long) arg;
+                postVerifyAuthentication(expectedUsername, postId);
+                break;      // 파라미터가 순서대로 들어가므로 MenuService의 update같이 타입이 같은게 있을 경우를 대비해 retaurantId만 받고 break한다.
+            }
+        }
+        return joinPoint.proceed();
+    }
+
+    /* 게시글에 대한 권환 확인 메서드 */
+    private void postVerifyAuthentication(String expectedUsername, Long postId) {
+
+        Post post = postRepository.findById(postId).orElseThrow(() ->
+                new CustomException(ErrorCode.POST_NOT_FOUND));
+
+        if (!expectedUsername.equals(post.getWriter().getUsername()))
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
+    }
 }
