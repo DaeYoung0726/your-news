10684093aa5b72d14551dd5e6c99da5a8f1e5ac8
DaeYoung Park <pdy000726@naver.com>
2024-06-02 22:14:24 +0900

    feat - post 기능 중 admin 관리 추가

--- src/main/java/project/yourNews/domains/post/service/AdminPostService.java
+++ src/main/java/project/yourNews/domains/post/service/AdminPostService.java
@@ -1,2 +1,26 @@
-package project.yourNews.domains.post.service;public class AdminPostService {
+package project.yourNews.domains.post.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.yourNews.domains.post.domain.Post;
+import project.yourNews.domains.post.repository.PostRepository;
+import project.yourNews.handler.exceptionHandler.error.ErrorCode;
+import project.yourNews.handler.exceptionHandler.exception.CustomException;
+
+@RequiredArgsConstructor
+@Service
+public class AdminPostService {
+
+    private final PostRepository postRepository;
+
+    /* 어드민이 글 삭제 */
+    @Transactional
+    public void deletePostByAdmin(Long postId) {
+
+        Post findPost = postRepository.findById(postId).orElseThrow(() ->
+                new CustomException(ErrorCode.POST_NOT_FOUND));
+
+        postRepository.delete(findPost);
+    }
 }
