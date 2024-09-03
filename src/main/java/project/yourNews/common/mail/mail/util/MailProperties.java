e145ba4e257f25772eae3063779a53fcd75a3e58
DaeYoung Park <pdy000726@naver.com>
2024-09-03 17:49:44 +0900

    refactor - 메일명 변경

--- src/main/java/project/yourNews/common/mail/mail/util/MailProperties.java
+++ src/main/java/project/yourNews/common/mail/mail/util/MailProperties.java
@@ -2,11 +2,11 @@ package project.yourNews.common.mail.mail.util;
 
 public interface MailProperties {
 
-    String CODE_SUBJECT = "[니 소식] 인증 메일입니다.";
-    String CODE_TEXT = "[니 소식] 인증번호는 ";
-    String NEWS_SUBJECT = "[니 소식] 새로운 소식입니다.";
-    String PASS_SUBJECT = "[니 소식] 임시 비밀번호입니다.";
+    String CODE_SUBJECT = "[YU소식] 인증 메일입니다.";
+    String CODE_TEXT = "[YU소식] 인증번호는 ";
+    String NEWS_SUBJECT = "[YU소식] 새로운 소식입니다.";
+    String PASS_SUBJECT = "[YU소식] 임시 비밀번호입니다.";
     String PASS_TEXT = "로그인 한 후 회원 정보 수정에서 비밀번호를 변경해 주세요. <br> 인증번호 : ";
-    String ASK_SUBJECT = "[니 소식] 문의 사항입니다.";
+    String ASK_SUBJECT = "[YU소식] 문의 사항입니다.";
     String ASK_TEXT = "문의자 이메일 : ";
 }
