8893d63e3a6af888a91ab71de8fd77c227f97c15
DaeYoung0726 <pdy000726@naver.com>
2024-10-08 02:49:12 +0900

    feat - 동시성 관리를 위한 bucket4j 설정

--- src/main/java/project/yourNews/common/crawling/strategy/YUNewsCrawlingStrategy.java
+++ src/main/java/project/yourNews/common/crawling/strategy/YUNewsCrawlingStrategy.java
@@ -1,5 +1,8 @@
 package project.yourNews.common.crawling.strategy;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,8 +16,8 @@ import project.yourNews.common.openAI.service.OpenAIService;
 import project.yourNews.domains.member.service.MemberService;
 import project.yourNews.domains.urlHistory.service.URLHistoryService;
 
+import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -25,12 +28,15 @@ public class YUNewsCrawlingStrategy implements CrawlingStrategy{
     private final OpenAIService openAIService;
 
     private String postTitle;
-    private int requestCount = 0;
 
     private static final String NEWS_NAME = "영대소식";
     private static final String MESSAGE_PREFIX =
             "키워드는 '장학금 및 학자금 지원', '취업', '교육 및 강좌', '학생복지', '연구 및 프로젝트 참여', '행사 및 설명회'가 있어." +
                     " 다음 제목의 키워드를 정확히 말해줘. 가장 유사한 하나의 키워드만 말해줘. 예를 들자면 '취업' 이렇게 말해. ";
+
+    private final Bucket bucket = Bucket.builder()
+            .addLimit(Bandwidth.classic(3, Refill.intervally(3, Duration.ofMinutes(1))))
+            .build();
 
     @Override
     public String getScheduledTime() {
@@ -73,7 +79,9 @@ public class YUNewsCrawlingStrategy implements CrawlingStrategy{
                 .role("user")
                 .content(messageContent)
                 .build();
-        manageRequestRate();
+
+        manageRequestRate();    // 외부 API 호출 횟수를 관리하기 위한 메서드
+
         ChatResponseDto gptResponse = openAIService.askQuestion(List.of(message));
 
         String keyword = gptResponse.getChoices().get(0).getMessage().getContent();
@@ -86,20 +94,15 @@ public class YUNewsCrawlingStrategy implements CrawlingStrategy{
         this.postTitle = postTitle;
     }
 
+    /* RateLimiter를 이용한 요청 횟수 제한 */
     private void manageRequestRate() {
-
-        if (requestCount > 0 && requestCount % 3 == 0) {
-            try {
-                Thread.sleep(TimeUnit.MINUTES.toMillis(1));
-                requestCount = 0;
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                throw new CustomException(ErrorCode.THREAD_INTERRUPTED);
-            }
+        try {
+            bucket.asBlocking().consume(1); // 토큰이 없으면 대기하여 요청 처리. 여기는 3으로 설정
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new CustomException(ErrorCode.THREAD_INTERRUPTED);
         }
-        requestCount++;
     }
-
 
     @Override
     public void saveURL(String postURL) {
