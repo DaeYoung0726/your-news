44dacd60e9499a11c980fab3fdda6560f3601dc0
DaeYoung0726 <pdy000726@naver.com>
2024-10-13 14:15:36 +0900

    refact: 인증 등록과정 수정

--- src/main/java/project/yourNews/security/auth/CustomDetailsService.java
+++ src/main/java/project/yourNews/security/auth/CustomDetailsService.java
@@ -16,14 +16,14 @@ public class CustomDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
 
     @Override
-    @Cacheable(value = "users", key = "#username")
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if(username == null || username.equals("")) {
-            throw new UsernameNotFoundException(username);
+    @Cacheable(value = "users", key = "#userId")
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        if (userId == null || userId.equals("")) {
+            throw new UsernameNotFoundException(userId);
         }
 
-        Member memberEntity = memberRepository.findByUsername(username).orElseThrow(() ->
-                new UsernameNotFoundException("User not found with username: " + username));
+        Member memberEntity = memberRepository.findById(Long.valueOf(userId)).orElseThrow(() ->
+                new UsernameNotFoundException("User not found with userId: " + userId));
 
         return new CustomDetails(memberEntity);
     }
